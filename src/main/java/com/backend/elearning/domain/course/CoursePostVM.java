5c9bebe36a42fb4b81f5b5e673eec708431c0241
Thuan ngo <thuanngo3072002@gmail.com>
2024-09-09 22:00:01 +0700

    authorize app by role

--- src/main/java/com/backend/elearning/domain/course/CoursePostVM.java
+++ src/main/java/com/backend/elearning/domain/course/CoursePostVM.java
@@ -1,10 +1,5 @@
 package com.backend.elearning.domain.course;
 
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-import java.util.Arrays;
-@EqualsAndHashCode
-@ToString
 public record CoursePostVM(
          Long id,
 
@@ -12,11 +7,11 @@ public record CoursePostVM(
 
          String headline,
 
-         @EqualsAndHashCode.Include String[] objectives,
+        String[] objectives,
 
-         @EqualsAndHashCode.Include String[] requirements,
+        String[] requirements,
 
-         @EqualsAndHashCode.Include String[] targetAudiences,
+        String[] targetAudiences,
 
          String description,
          String level,
@@ -27,28 +22,5 @@ public record CoursePostVM(
          Integer topicId
 
 ) {
-    @EqualsAndHashCode.Include
-    private boolean arraysEqual(String[] array1, String[] array2) {
-        return Arrays.equals(array1, array2);
-    }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof CoursePostVM other)) return false;
-
-        return id.equals(other.id) &&
-                title.equals(other.title) &&
-                headline.equals(other.headline) &&
-                arraysEqual(objectives, other.objectives) &&
-                arraysEqual(requirements, other.requirements) &&
-                arraysEqual(targetAudiences, other.targetAudiences) &&
-                description.equals(other.description) &&
-                level.equals(other.level) &&
-                price.equals(other.price) &&
-                image.equals(other.image) &&
-                free == other.free &&
-                categoryId.equals(other.categoryId) &&
-                topicId.equals(other.topicId);
-    }
 }
