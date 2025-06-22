eac3d7a896c86d30d5802a405dfd44e2eb54ca76
Thuan ngo <thuanngo3072002@gmail.com>
2025-06-22 23:29:08 +0700

    review chat realtime

--- src/main/java/com/backend/elearning/domain/chat/MessageController.java
+++ src/main/java/com/backend/elearning/domain/chat/MessageController.java
@@ -1,6 +1,7 @@
 package com.backend.elearning.domain.chat;
 
 import com.backend.elearning.domain.questionLecture.AnswerLecture;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
@@ -8,15 +9,16 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Slf4j
 public class MessageController {
     private final SimpMessagingTemplate messagingTemplate;
 
     public MessageController(SimpMessagingTemplate messagingTemplate) {
         this.messagingTemplate = messagingTemplate;
     }
-    @MessageMapping("/chat/{questionId}") // Match room-specific endpoints
+    @MessageMapping("/chat/{questionId}")
     public void sendMessage(@DestinationVariable String questionId, AnswerLecture message) {
-        // Broadcast message to room-specific topic
+        log.info("received questionId: {}, answer content: {}", questionId, message);
         messagingTemplate.convertAndSend("/topic/questions/" + questionId, message);
     }
 
